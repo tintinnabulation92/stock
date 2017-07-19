21fa399dece7aa0455f8503dd0d6df6add0949ea
pawel.holz <pawel.holz@jitsolutions.pl>
2017-07-19 15:18:34 +0200

    All changes complete 19.09 15:18

--- src/components/offerPresenterComponent/offerPresenterComponent.jsx
+++ src/components/offerPresenterComponent/offerPresenterComponent.jsx
@@ -1,7 +1,6 @@
 import React, {PropTypes} from "react";
 import Modal from 'react-modal';
 import {Link} from 'react-router';
-import OfferDetailsComponent from '../offerDetailsComponent/offerDetailsComponent' // wyjebac to
 import cls from "./offerPresenterComponent.css"
 
 
